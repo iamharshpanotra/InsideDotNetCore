fd22dadaf97e57de5ea442486e1fb36c6970c2a9
iamharshpanotra <sharmaharsh506325@gmail.com>
2025-02-03 21:13:25 +0530

    Initial Commit

---
+++ TestApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
