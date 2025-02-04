86a9caf3b9c24e74c222d71eaaf6c3fdbaf3857e
Harsh <harsh.sharma@maxworthsystems.com>
2025-02-04 17:55:44 +0530

    CRUD Operations

---
+++ TestApp1/Services/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestApp1.Models;
+
+namespace TestApp1.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
